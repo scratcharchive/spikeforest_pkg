fddc9ccc14e31e3b104254975418b176bf3e68cc
Jeremy Magland <jeremy.magland@gmail.com>
2019-04-23 09:22:09 -0400

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ pkg_name = "spikeforest"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.2.2",
+    version="0.2.3",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Spike sorting",
